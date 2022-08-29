b3823b493014a036d92cd5885a6f4d6b0d3d165e
Amartyaqaz18 <mr.tripathiamartya@gmail.com>
2022-08-29 11:03:19 +0530

    Initial commit

---
+++ src/Components/Banner.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+//import {movies} from './getMovies';
+import axios from 'axios';
+export default class Banner extends Component {
+  constructor(){
+    super();
+    this.state={
+        movies:[],
+        currPage:1
+    }
+}
+  async componentDidMount(){
+    const res=await axios.get(`https://api.themoviedb.org/3/trending/all/day?api_key=6e2e1c999ae47325ddc250a51ea62060&language=en-US&page=${this.state.currPage}`)
+    let data=res.data
+    this.setState({
+      movies:[...data.results]
+    })
+  }  
+
+  render() {
+    
+    let movie=this.state.movies
+    
+    return (
+        <>
+        {
+            movie==''?
+            <div class="spinner-border text-primary" role="status">
+            <span class="visually-hidden">Loading...</span>
+            </div>:
+            <div className="card banner-card">
+            <img src={`https://image.tmdb.org/t/p/original${movie[0].backdrop_path}`}   alt={movie[0].title} className="card-img-top banner-img"/>
+            {/*<div class="card-body">*/}
+              <h1 className="card-title banner-title">{movie[0].original_title}{movie[0].original_name}</h1>
+              <p className="card-text banner-text">{movie[0].overview}</p>
+              {/* <a href="#" class="btn btn-primary">Go somewhere</a> */}
+            {/*</div>*/}
+          </div>
+  }
+      </>
+    )
+  }
+}
