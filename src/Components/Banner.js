2618b8209c39998998e1bb500cfade7204750fb6
Amartyaqaz18 <87028267+Amartyaqaz18@users.noreply.github.com>
2023-12-03 22:02:17 +0530

    Update Banner.js

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -29,10 +29,10 @@ export default class Banner extends Component {
             <span class="visually-hidden">Loading...</span>
             </div>:
             <div className="card banner-card">
-            <img src={`https://image.tmdb.org/t/p/original${movie[0].backdrop_path}`}   alt={movie[0].title} className="card-img-top banner-img"/>
+            <img src={`https://image.tmdb.org/t/p/original${movie[1].backdrop_path}`}   alt={movie[1].title} className="card-img-top banner-img"/>
             {/*<div class="card-body">*/}
-              <h1 className="card-title banner-title">{movie[0].original_title}{movie[0].original_name}</h1>
-              <p className="card-text banner-text">{movie[0].overview}</p>
+              <h1 className="card-title banner-title">{movie[1].original_title}{movie[1].original_name}</h1>
+              <p className="card-text banner-text">{movie[1].overview}</p>
               {/* <a href="#" class="btn btn-primary">Go somewhere</a> */}
             {/*</div>*/}
           </div>
